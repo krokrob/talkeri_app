102e16c9660a5f226a8710d6225ef47b4999a74f
Matthieu Rousseau <matthieurousseau86@gmail.com>
2018-08-28 15:56:32 +0200

    Chat working

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -16,4 +16,27 @@ class ChatroomsController < ApplicationController
     @active_alert = @chatroom.alerts.where(status: false)
   end
 
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+
+    @user_chatroom = UserChatroom.new(chatroom_params)
+    # @message.chatroom = @
+    # @message.user = current_user
+    # if @message.save
+    #   redirect_to chatroom_path(@chatroom)
+    # else
+    #   render :new
+    # end
+  end
+
+  def one_on_one_chatroom
+
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:user)
+  end
+
 end
