a4fd65fdceb272ac32b3666fc3c0544e724222a9
Matthieu Rousseau <matthieurousseau86@gmail.com>
2018-08-28 12:04:15 +0200

    Fixed the message input form

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   resources :chatrooms do
     resources :messages
   end
-  get '/chatrooms/:chatroom_id/messages', to: 'messages#create', as: :create_message
   get '/design', to: 'pages#design'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
