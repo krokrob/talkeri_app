b87a35e4d9097614de5ab330e6da64d0a0cd5823
Emilie Deschamps-Wright <e.wright@hotmail.fr>
2018-08-30 15:33:32 +0200

    sidebaryouhou

--- app/javascript/components/sidebar.js
+++ app/javascript/components/sidebar.js
@@ -1,9 +1,15 @@
 function hideSidebar() {
   const sidebar = document.getElementById("sidebar");
   const logo = document.getElementById("logo");
-logo.addEventListener('click', (event) => {
+  const logo_closing = document.getElementById("logo-closing")
+  logo.addEventListener('click', (event) => {
   sidebar.classList.toggle('sidebar-collapse');
-})
+  sidebar.classList.toggle('absolute');
+  });
+  logo_closing.addEventListener('click', (event) => {
+    sidebar.classList.toggle('sidebar-collapse');
+    sidebar.classList.toggle('absolute');
+  });
 }
 
 export {hideSidebar};
