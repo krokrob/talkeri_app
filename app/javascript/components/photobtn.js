fb0a28878010e0df379e3fb809fc2e3f3302a4d1
sami bekas <samibekas@gmail.com>
2018-08-31 11:18:17 +0200

    frontend updates

--- app/javascript/components/photobtn.js
+++ app/javascript/components/photobtn.js
@@ -2,12 +2,14 @@ function photoBtn() {
   const photoButton = document.getElementById('photo-btn')
   const cloudinaryInput = document.getElementById('message_photo')
   const submitSimpleForm = document.getElementById("submit-text");
-  photoButton.addEventListener( 'click', (event) => {
-    cloudinaryInput.click();
-    cloudinaryInput.addEventListener( 'change', (event) => {
-      submitSimpleForm.click();
+  if (photoButton) {
+    photoButton.addEventListener( 'click', (event) => {
+      cloudinaryInput.click();
+      cloudinaryInput.addEventListener( 'change', (event) => {
+        submitSimpleForm.click();
+      });
     });
-  });
+  }
 }
 
 export { photoBtn }
