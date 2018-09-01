2d7d3aabb403ff00c5d293812e08e92ca46075b4
sami bekas <samibekas@gmail.com>
2018-09-01 13:47:30 +0200

    little details

--- app/javascript/components/messagescrolldown.js
+++ app/javascript/components/messagescrolldown.js
@@ -1,14 +1,10 @@
-
-
 function scrollMessages() {
   const messagesContainer = document.querySelectorAll('.message');
-  console.log(messagesContainer);
   if (messagesContainer.length > 0) {
     const messages = document.querySelectorAll('.message');
     const lastMessage = messages[messages.length - 1];
     if (lastMessage) {
       lastMessage.scrollIntoView();
-    console.log('scroll');
     }
   }
 }
