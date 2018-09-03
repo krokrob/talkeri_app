7aa9268fa5bf2c9ddbab6de34dad351516102be5
sami bekas <samibekas@gmail.com>
2018-09-03 18:17:56 +0200

    enfin

--- app/javascript/components/photobtn.js
+++ app/javascript/components/photobtn.js
@@ -1,13 +1,9 @@
 function photoBtn() {
   const photoButton = document.getElementById('photo-btn');
   const cloudinaryInput = document.getElementById('message_photo');
-  const submitSimpleForm = document.getElementById("submit-photo");
   if (photoButton) {
-    // photoButton.addEventListener( 'click', (event) => {
-      // cloudinaryInput.click();
-      cloudinaryInput.addEventListener( 'change', (event) => {
-        document.getElementById('photo-form').submit();
-      // });
+    cloudinaryInput.addEventListener( 'change', (event) => {
+      document.getElementById('photo-form').submit();
     });
   }
 }
