dd602266057e57b0d3f2aba626db54735123c7b5
sami bekas <samibekas@gmail.com>
2018-09-04 11:21:49 +0200

    speaker face displayed

--- app/javascript/components/audioplayer.js
+++ app/javascript/components/audioplayer.js
@@ -1,9 +1,9 @@
-function audioPlayer() {
-  if (document.querySelector(".player")) {
-    const audios = document.querySelectorAll(".player");
-    const size = audios.length;
-    // audios[size - 1].play();
-  };
-}
+// function audioPlayer() {
+//   if (document.querySelector(".player")) {
+//     const audios = document.querySelectorAll(".player");
+//     const size = audios.length;
+//     audios[size - 1].play();
+//   };
+// }
 
-export { audioPlayer };
+// export { audioPlayer };
